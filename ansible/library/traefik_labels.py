e6260d5441c6474ca74e0e5ae410d1ddbc995a10
Mathieu Garcia <mathieu.garcia@ovhcloud.com>
2025-01-29 11:34:35 +0100

    feat(ansible): Move traefik_labels lib to nomad

--- ansible/library/traefik_labels.py
+++ ansible/library/traefik_labels.py
@@ -3,21 +3,19 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from ansible.module_utils.basic import AnsibleModule
-from passlib.hash import bcrypt
-
 
 def main():
     module_args = dict(
         proto=dict(type='str', choices=['tcp', 'http'], required=True),
         domain=dict(type='str', required=True),
         domain_alias=dict(type='str', required=False, default=None),
-        basic_auth=dict(type='bool', required=False, default=False),
+        basic_auth=dict(type='str', required=False, default=None),
         ipfilter=dict(type='list', elements='str', required=False, default=[])
     )
 
     result = dict(
         changed=False,
-        labels={}
+        labels=[]
     )
 
     module = AnsibleModule(
@@ -70,29 +68,23 @@ def main():
 
     # Basic auth
     if basic_auth:
-        # Generate basic_auth string from environment variables
-        project_admin_login = module.run_command("printenv PROJECT_ADMIN_LOGIN")[1].strip()
-        project_admin_password = module.run_command("printenv PROJECT_ADMIN_PASSWORD")[1].strip()
-        if not project_admin_login or not project_admin_password:
-            module.fail_json(msg="Environment variables PROJECT_ADMIN_LOGIN or PROJECT_ADMIN_PASSWORD are not set")
-
-        hashed_password = bcrypt.hash(project_admin_password)
-        auth_string = f"{project_admin_login}:{hashed_password}"
-
-        labels[f"traefik.http.middlewares.{service_name}-basicauth.basicauth.users"] = auth_string
-        middlewares.append(f"{service_name}-basicauth@docker")
+        labels[f"traefik.http.middlewares.{service_name}-basicauth.basicauth.users"] = basic_auth
+        middlewares.append(f"{service_name}-basicauth@nomad")
 
     # IP Filtering
     if ipfilter:
         labels[f"traefik.http.middlewares.{service_name}-whistelist.IPAllowList.sourcerange"] = ",".join(ipfilter)
-        middlewares.append(f"{service_name}-whistelist@docker")
+        middlewares.append(f"{service_name}-whistelist@nomad")
 
     # Add middlewares to the router if any exist
     if middlewares:
         labels[f"traefik.http.routers.{service_name}.middlewares"] = ",".join(middlewares)
 
+    # Convert labels dict to a list of "key=value" strings
+    labels_list = [f"{key}={value}" for key, value in labels.items()]
+
     # Return the labels
-    result['labels'] = labels
+    result['labels'] = labels_list
 
     module.exit_json(**result)
 
