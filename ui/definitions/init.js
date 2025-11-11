b4d308321e85ba3e09282ed1641728a23a690f25
Mathieu Garcia <mgarcia@wiseflat.com>
2025-11-11 15:03:52 +0100

    refactor(ui): remove unused user initialization

--- ui/definitions/init.js
+++ ui/definitions/init.js
@@ -1,14 +1,4 @@
 CONF.$customtitles = true;
 CONF.version = '1.1';
 
-MAIN.users = [];
-
-(async function() {
-	var tmp = [];
-	for (let m of tmp) {
-		if (m !== null && m !== 'bot')
-			MAIN.users.push(m);
-	}
-})();
-
 LOCALIZE(ctrl => ctrl.query.language || (ctrl.user ? ctrl.user.language : CONF.language));
